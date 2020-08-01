8343d9f70565ff19686dffc0fac051e883491d52
Daichi Kudo <sakanactor624@gmail.com>
2020-08-01 16:52:31 +0900

    :tada: initial commit

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-(function(){
-    console.log("hello treasure");
+(async function () {
+  console.log("hello treasure");
+  let localStream;
+  try {
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, voice: true });
+    const videoElement = document.getElementById("my-video");
+    videoElement.srcObject = localStream;
+    videoElement.play();
+  } catch (error) {
+    alert(error);
+  }
+  const peer = new Peer({
+    key: "a03deb72-2a82-459d-b1f8-76f6a6f1c9a2",
+    debug: 3,
+  });
+  // PeerID取得
+  peer.on("open", () => {
+    document.getElementById("my-id").textContent = peer.id;
+  });
+  // 発信処理
+  document.getElementById("make-call").onclick = () => {
+    const theirID = document.getElementById("their-id").value;
+    const mediaConnection = peer.call(theirID, localStream);
+    setEventListener(mediaConnection);
+  };
+  // イベントリスナ
+  const setEventListener = (mediaConnection) => {
+    mediaConnection.on("stream", (stream) => {
+      const videoElement = document.getElementById("their-video");
+      videoElement.srcObject = stream;
+      videoElement.play();
+    });
+  };
+  // 着信処理
+  peer.on("call", (mediaConnection) => {
+    mediaConnection.answer(localStream);
+    setEventListener(mediaConnection);
+  });
 })();
