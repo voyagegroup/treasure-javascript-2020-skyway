765b2f95a0b99d59f54a985371b66bde94e6353a
Daiki <daiki.minobe@gmail.com>
2020-08-01 16:40:43 +0900

    create skyway video service

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
-(function(){
+
+(async function(){
     console.log("hello treasure");
+    let localStream;
+    try {
+        localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        const videoElement = document.getElementById('my-video');
+        videoElement.srcObject = localStream;
+        videoElement.play();
+        
+    }catch(error){
+        alert(error);
+    }
+
+    //Peer作成
+    const peer = new Peer({
+        key: '0465b4b9-6179-47ab-9487-b8bd6ca62eba',
+        debug: 3
+    });
+
+    peer.on('open', () => {
+        document.getElementById('my-id').textContent = peer.id;
+    });
+
+    // // 発信処理
+    document.getElementById('make-call').onclick = () => {
+        const theirID = document.getElementById('their-id').value;
+        const mediaConnection = peer.call(theirID, localStream);
+        setEventListener(mediaConnection);
+    };
+    
+    // イベントリスナを設置する関数
+    const setEventListener = mediaConnection => {
+        mediaConnection.on('stream', stream => {
+        // video要素にカメラ映像をセットして再生
+        const videoElm = document.getElementById('their-video')
+        videoElm.srcObject = stream;
+        videoElm.play();
+        });
+    }
+
+    //着信処理
+    peer.on('call', mediaConnection => {
+        mediaConnection.answer(localStream);
+        setEventListener(mediaConnection);
+    });
+
+
+    
 })();
+
