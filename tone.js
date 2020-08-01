f30dc27e414d4f9ef30e27ea0f17e97f06cbf97d
Tanoue Ryo <shg18.sk1.hr10@gmail.com>
2020-08-01 17:51:16 +0900

    add voice changer

--- tone.js
+++ tone.js
@@ -1,3 +1,4 @@
+const Tone = require('tone');
 // マイクを用意し、ONにする
 const micAudio = new Tone.UserMedia();
 micAudio.open();
@@ -5,7 +6,7 @@ micAudio.open();
 // testボタン押下のイベントリスナ
 document.getElementById('voice_change').addEventListener('click', () => {
   // ピッチシフター（音程を変更するエフェクタ）を用意。音の高さは5。
-  const shifter = new Tone.PitchShift(5);
+  const shifter = new Tone.PitchShift(10);
   // マスターオーディオ（スピーカー）に接続された、リバーブ（残響効果エフェクタ）を用意。
   const reverb = new Tone.Freeverb().toMaster();
   // マイク音声をピッチシフターに接続
