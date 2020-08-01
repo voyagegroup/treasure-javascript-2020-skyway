18a324ed79032d6dbddffe78f5d4d9fe6a2269ce
Ogijun2018 <ogijun.mk@hotmail.co.jp>
2020-08-01 16:46:23 +0900

    skyway導入

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-(function(){
+(async function(){
     console.log("hello treasure");
+    let localStream;
+    try{
+        localStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+        const videoElement = document.getElementById('my-video');
+        videoElement.srcObject = localStream;
+        videoElement.play();
+    } catch(error) {
+        alert(error);
+    }
+
+    //Peer作成
+    const peer = new Peer({
+        key: 'ea80ddcc-69d3-4f03-a981-75bb74a3f4bb',
+        debug: 3
+    });
+
+    //PeerID取得
+    peer.on('open', () => {
+        document.getElementById('my-id').textContent = peer.id;
+    });
+
+    // 発信処理
+    document.getElementById('make-call').onclick = () => {
+        const theirID = document.getElementById('their-id').value;
+        const mediaConnection = peer.call(theirID, localStream);
+        setEventListener(mediaConnection);
+    };
+
+    //着信処理
+    peer.on('call', mediaConnection => {
+        mediaConnection.answer(localStream);
+        setEventListener(mediaConnection);
+    });
+    
+    // イベントリスナを設置する関数
+    const setEventListener = mediaConnection => {
+        mediaConnection.on('stream', stream => {
+        // video要素にカメラ映像をセットして再生
+        const videoElm = document.getElementById('their-video')
+        videoElm.srcObject = stream;
+        videoElm.play();
+        });
+    }
+
 })();
