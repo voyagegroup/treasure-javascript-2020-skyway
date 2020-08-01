55b92ece69c06e60712ba142e1f1d80cfb62045e
tarao1006 <taiga4562@gmail.com>
2020-08-01 21:16:30 +0900

    Add text chat skeleton

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
 
     const peer = new Peer({
         key: "17f87a7c-2f92-4747-b274-d65882a0ebdb",
-        debug: 3
+        debug: 0
     })
 
     peer.on('open', () => {
@@ -27,10 +27,25 @@
         setEventListener(mediaConnection);
     })
 
+    peer.on('connection', dataConnection => {
+        setEventListenerForData(dataConnection);
+    })
+
     document.getElementById('make-call').onclick = () => {
         const theirID = document.getElementById('their-id').value;
         const mediaConnection = peer.call(theirID, localStream);
+        const dataConnection = peer.connect(theirID);
+
         setEventListener(mediaConnection);
+        setEventListenerForData(dataConnection);
+
+        document.getElementById('send-message').onclick = () => {
+            const text = document.getElementById('local-text').value;
+            dataConnection.send(text);
+      
+            document.getElementById('messages').textContent.textContent += `You: ${text}\n`;
+            document.getElementById('local-text').value = '';
+        }
 
         document.getElementById('close-call').onclick = () => {
             mediaConnection.close(true);
@@ -43,6 +58,20 @@
             const videoElement = document.getElementById('their-video');
             videoElement.srcObject = stream;
             videoElement.play();
+        })
+    }
+
+    const setEventListenerForData = dataConnection => {
+        dataConnection.on('open', async () => {
+            console.log('open')
+            document.getElementById('messages').textContent += "open\n";
+        });
+        dataConnection.on('data', data => {
+            console.log(data)
+            document.getElementById('messages').textContent += `Remote: ${data}\n`;
+        });
+        dataConnection.on('error', err => {
+            console.log(err)
         })
     }
 })();
