ed8d0f5a93732ad5242e24e600e470da574cd148
Daichi Kudo <sakanactor624@gmail.com>
2020-08-01 17:07:27 +0900

    audio true

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
   console.log("hello treasure");
   let localStream;
   try {
-    localStream = await navigator.mediaDevices.getUserMedia({ video: true, voice: true });
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
     const videoElement = document.getElementById("my-video");
     videoElement.srcObject = localStream;
     videoElement.play();
