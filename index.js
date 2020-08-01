8a526570dca61cb46e59f7d74bcd913ac93ffb8c
yayoi03 <uni.ani367@gmail.com>
2020-08-01 16:48:24 +0900

    skyway connection

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-(function(){
+(async function(){
     console.log("hello treasure");
+    let localStream;
+    try {
+        localStream = await navigator.mediaDevices.getUserMedia({ video: true, voice: true });
+        const videoElement = document.getElementById('my-video');
+        videoElement.srcObject = localStream;
+        videoElement.play();
+    } catch(error) {
+        alert(error);
+    }
+    const peer = new Peer({
+        key: '781c884a-f70d-49bf-9860-b1593057e647',
+        debug: 3
+    });
+    // PeerID取得
+    peer.on('open', () => {
+        document.getElementById('my-id').textContent = peer.id;
+    });
+    // 発信処理
+    document.getElementById('make-call').onclick = () => {
+        const theirID = document.getElementById('their-id').value;
+        const mediaConnection = peer.call(theirID, localStream);
+        setEventListener(mediaConnection);
+    };
+    // イベントリスナ
+    const setEventListener = mediaConnection => {
+        mediaConnection.on('stream', stream => {
+            const videoElement = document.getElementById('their-video');
+            videoElement.srcObject = stream;
+            videoElement.play();
+        });
+    };
+    // 着信処理
+    peer.on('call', mediaConnection => {
+        mediaConnection.answer(localStream);
+        setEventListener(mediaConnection);
+    });
 })();
