8eb8fd8c923107f0c927cd4e9f2daaf142999237
Yohei Maeda <mdyh2a380824@gmail.com>
2020-08-01 17:12:35 +0900

    modify

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-(function(){
+(async function(){
     console.log("hello treasure");
+    let localStream;
+    try {
+        localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        const videoElement = document.getElementById('my-video');
+        videoElement.srcObject = localStream;
+        videoElement.play();
+    } catch(error) {
+        alert(error);
+    }
+
+    const peer = new Peer({
+        key: '5c7b07d3-5053-4cbf-a699-cb4caed4e344',
+        debug: 3
+    });
+
+    peer.on('open', () => {
+        document.getElementById('my-id').textContent = peer.id;
+    });
+
+    document.getElementById('make-call').onclick = () => {
+        const theirID = document.getElementById('their-id').value;
+        const mediaConnection = peer.call(theirID, localStream);
+        setEventListener(mediaConnection);
+    };
+
+    const setEventListener = mediaConnection => {
+        mediaConnection.on('stream', stream => {
+            const videoElm = document.getElementById('their-video');
+            videoElm.srcObject = stream;
+            videoElm.play();
+        });
+    }
+
+    peer.on('call', mediaConnection => {
+        mediaConnection.answer(localStream);
+        setEventListener(mediaConnection);
+    });
+
 })();
