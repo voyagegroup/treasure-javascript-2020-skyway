1f6b28c359d8bb97c1e4f43db9764feaddd8c475
KazuyaMatsunaga <capr1net.wildcard@gmail.com>
2020-08-01 16:59:09 +0900

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-(function(){
-    console.log("hello treasure");
+(async function() {
+  console.log("hello treasure");
+  let localStream;
+  try {
+    localStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    const videoElement = document.getElementById("my-video");
+    videoElement.srcObject = localStream;
+    videoElement.play();
+  } catch (error) {
+    alert(error);
+  }
+
+  //Peer作成
+  const peer = new Peer({
+    key: "c1f3ecfd-8244-4e62-8672-9827afcd0e9a",
+    debug: 3,
+  });
+
+  //PeerID取得
+  peer.on("open", () => {
+    document.getElementById("my-id").textContent = peer.id;
+  });
+
+  // 発信処理
+  document.getElementById("make-call").onclick = () => {
+    const theirID = document.getElementById("their-id").value;
+    const mediaConnection = peer.call(theirID, localStream);
+    setEventListener(mediaConnection);
+  };
+
+  // イベントリスナを設置する関数
+  const setEventListener = (mediaConnection) => {
+    mediaConnection.on("stream", (stream) => {
+      // video要素にカメラ映像をセットして再生
+      const videoElm = document.getElementById(`their-video`);
+      videoElm.srcObject = stream;
+      videoElm.play();
+    });
+  };
+
+  //着信処理
+  peer.on("call", (mediaConnection) => {
+    mediaConnection.answer(localStream);
+    setEventListener(mediaConnection);
+  });
 })();
