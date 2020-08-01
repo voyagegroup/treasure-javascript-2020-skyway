706aea74c2e365931810075eb07c0ac51cff3895
svpcadmin <PhilisPaxil@gmail.com>
2020-08-01 16:56:47 +0900

    add: connection

--- src/views/pages/Home.tsx
+++ src/views/pages/Home.tsx
@@ -1,25 +1,69 @@
 import * as React from "react";
 import { useUserMedia } from "../../hooks/useUserMedia";
+import { usePeer } from "../../hooks/usePeer";
+import { requestDispatch } from "../../modules/requestDispatch/requestDispath";
 
 export const Home: React.FC<{}> = () => {
+  const [peerId, setPeerId] = React.useState("");
+  const [userPeerId, setUserPeerId] = React.useState("");
+  const peer = usePeer();
+
+  let localStream: MediaStream;
+
   const videoEl = React.useRef<HTMLVideoElement>(null);
+  const peerVideoEl = React.useRef<HTMLVideoElement>(null);
+
   React.useEffect(() => {
-    useUserMedia()
-      .then((stream) => {
-          
-        const videoElm = videoEl.current!;
-        videoElm.srcObject = stream;
-        videoElm.play();
-      })
-      .catch((error) => {
-        console.error("mediaDevice.getUserMedia() error:", error);
-        return;
+    peer.on("open", () => {
+      setUserPeerId(peer.id);
+
+      // TODO: あとでなんとかする, 汚い
+      peer.on("call", (mediaConnection) => {
+        mediaConnection.answer(localStream);
+        mediaConnection.on("stream", (stream) => {
+            const videoElm = peerVideoEl.current!;
+            videoElm.srcObject = stream;
+            videoElm.play();
+          });
       });
+    });
+
+    useUserMedia().then((stream) => {
+      const videoElm = videoEl.current!;
+      videoElm.srcObject = stream;
+      videoElm.play();
+      localStream = stream;
+    });
   }, []);
+
+  // TODO: 関心の分離
+  const requestDis = () => {
+    const connection = peer.call(peerId, localStream);
+    connection.on("stream", (stream) => {
+      const videoElm = peerVideoEl.current!;
+      videoElm.srcObject = stream;
+      videoElm.play();
+    });
+  };
 
   return (
     <div>
-      Hello <video ref={videoEl} />
+      Hello {userPeerId}
+      <div>
+        <input
+          type="text"
+          value={peerId}
+          onChange={(e) => setPeerId(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            requestDis();
+          }}>
+          発信
+        </button>
+      </div>{" "}
+      <video ref={videoEl} />
+      <video ref={peerVideoEl} />
     </div>
   );
 };
