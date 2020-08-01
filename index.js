7146b425aca9eb32c77722bc5e44e5e4dc1d5a8c
Shunya078 <kikukan007@gmail.com>
2020-08-01 16:46:32 +0900

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-(function(){
+(async function () {
     console.log("hello treasure");
+    let localStream;
+    try {
+        const localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        const videoElement = document.getElementById('my-video');
+        videoElement.srcObject = localStream;
+        videoElement.play();
+    } catch (error) {
+        alert(error);
+    }
+    const peer = new Peer({
+        key: 'd35f0980-bf6c-4734-97cc-5e7ede92e388',
+        debug: 3
+    });
+
+    peer.on('open', () => {
+        document.getElementById('my-id').textContent = peer.id;
+    });
+
+    //以下発信処理
+    document.getElementById('make-call').onclick = () => {
+        const theirID = document.getElementById('their-id').value;
+        const mediaConnection = peer.call(theirID, localStream);
+        setEventLister(mediaConnection);
+    }
+
+    const setEventLister = mediaConnection => {
+        mediaConnection.on('stream', stream => {
+            const videoElement = document.getElementById('their-video')
+            videoElement.srcObject = stream;
+            videoElement.play();
+        })
+    }
+
+    //以下着信処理
+    peer.on('call', mediaConnection => {
+        mediaConnection.answer(localStream);
+        setEventLister(mediaConnection);
+    });
 })();
