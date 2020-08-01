5115dae0cc37b5d1f590f2f0702eb955824a951c
t-hoso <taishihosokawa@yahoo.co.jp>
2020-08-01 00:17:03 -0700

    fixed to use video

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-(function(){
+(async function(){
     console.log("hello treasure");
+    try {
+        const localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        const videoElement = document.getElementById('my-video');
+        videoElement.srcObject = localStream;
+        videoElement.play();
+    } catch(error) {
+        alert(error);
+    }
 })();
