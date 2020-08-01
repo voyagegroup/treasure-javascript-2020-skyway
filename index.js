b8cb7985c26dccaad9d70f1b050ef4686677a0a6
Daichi Kudo <sakanactor624@gmail.com>
2020-08-01 21:16:33 +0900

    :sparkles: 2020-08-01

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// 今週中にpull requestを投げる！
+
 (async function () {
   console.log("hello treasure");
   let localStream;
@@ -13,17 +15,17 @@
     key: "1212399c-448f-4135-89d3-76deff99795a",
     debug: 3,
   });
-  // PeerID取得
+
   peer.on("open", () => {
     document.getElementById("my-id").textContent = peer.id;
   });
-  // 発信処理
+
   document.getElementById("make-call").onclick = () => {
     const theirID = document.getElementById("their-id").value;
     const mediaConnection = peer.call(theirID, localStream);
     setEventListener(mediaConnection);
   };
-  // イベントリスナ
+
   const setEventListener = (mediaConnection) => {
     mediaConnection.on("stream", (stream) => {
       const videoElement = document.getElementById("their-video");
@@ -31,9 +33,50 @@
       videoElement.play();
     });
   };
-  // 着信処理
+
   peer.on("call", (mediaConnection) => {
     mediaConnection.answer(localStream);
     setEventListener(mediaConnection);
+  });
+
+  // コピペ
+  const localId = document.getElementById("js-local-id");
+  const localText = document.getElementById("js-local-text");
+  const connectTrigger = document.getElementById("js-connect-trigger");
+  const closeTrigger = document.getElementById("js-close-trigger");
+  const sendTrigger = document.getElementById("js-send-trigger");
+  const remoteId = document.getElementById("js-remote-id");
+  const messages = document.getElementById("js-messages");
+  const meta = document.getElementById("js-meta");
+  const sdkSrc = document.querySelector("script[src*=skyway]");
+
+  connectTrigger.addEventListener("click", () => {
+    const dataConnecton = peer.connect(remoteId.value);
+
+    dataConnection.on("data", (data) => {
+      messages.textContent += `Remote: ${data}\n`;
+    });
+
+    dataConnecton.once("close", () => {
+      messages.textContent += `Remote: ${data}\n`;
+    });
+
+    dataConnection.once("close", () => {
+      messages.textContent += `=== DataConnection has been closed ===\n`;
+      sendTrigger.removeEventListener("click", onClickSend);
+    });
+
+    // Register closing handler
+    closeTrigger.addEventListener("click", () => dataConnection.close(), {
+      once: true,
+    });
+
+    function onClickSend() {
+      const data = localText.value;
+      dataConnection.send(data);
+
+      messages.textContent += `You: ${data}\n`;
+      localText.value = "";
+    }
   });
 })();
