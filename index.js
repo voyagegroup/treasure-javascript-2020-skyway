be5f25eee2bfd3da73403d64c55c58b0b49d27c9
Prog24 <kenta@suzuki.dev>
2020-08-01 21:11:49 +0900

    tmp

--- index.js
+++ index.js
@@ -14,32 +14,55 @@
         key: 'f43b2465-bc02-4d13-b7af-ed584aa2451e',
         debug: 3
     });
-    //PeerID取得
-    peer.on('open', () => {
-        document.getElementById('my-id').textContent = peer.id;
-    });
+    // //PeerID取得
+    // peer.on('open', () => {
+    //     document.getElementById('my-id').textContent = peer.id;
+    // });
 
-    // 発信処理
+    console.log(">>>>>>>>>>>")
+    console.log(peer)
     document.getElementById('make-call').onclick = () => {
-        const theirID = document.getElementById('their-id').value;
-        const mediaConnection = peer.call(theirID, localStream);
-        setEventListener(mediaConnection);
-    };
-
-    // イベントリスナを設置する関数
-    const setEventListener = mediaConnection => {
-        mediaConnection.on('stream', stream => {
-            // video要素にカメラ映像をセットして再生
-            const videoElm = document.getElementById('their-video')
-            videoElm.srcObject = stream;
-            videoElm.play();
+        const meshRoom = peer.joinRoom('room', {
+            mode: 'mesh',
+            stream: localStream,
+        })
+        meshRoom.on('open', () => {
+            console.log(`join ${peer.id}`)
+        })
+        meshRoom.on('peerJoin', peerId => {
+            console.log(`plus join ${peerId}`)
         });
     }
 
-    //着信処理
-    peer.on('call', mediaConnection => {
-        mediaConnection.answer(localStream);
-        setEventListener(mediaConnection);
-    });
+    // const meshRoom = peer.joinRoom('roomName', {
+    //     mode: 'mesh',
+    //     stream: localStream,
+    // });
+    // meshRoom.once('open', () => {
+    //     console.log("hoge")
+    // })
+
+    // // 発信処理
+    // document.getElementById('make-call').onclick = () => {
+    //     const theirID = document.getElementById('their-id').value;
+    //     const mediaConnection = peer.call(theirID, localStream);
+    //     setEventListener(mediaConnection);
+    // };
+
+    // // イベントリスナを設置する関数
+    // const setEventListener = mediaConnection => {
+    //     mediaConnection.on('stream', stream => {
+    //         // video要素にカメラ映像をセットして再生
+    //         const videoElm = document.getElementById('their-video')
+    //         videoElm.srcObject = stream;
+    //         videoElm.play();
+    //     });
+    // }
+
+    // //着信処理
+    // peer.on('call', mediaConnection => {
+    //     mediaConnection.answer(localStream);
+    //     setEventListener(mediaConnection);
+    // });
 
 })();
