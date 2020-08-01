cfb0fd96974d3252c21710cc014caf1d6269d9bf
Prog24 <kenta@suzuki.dev>
2020-08-01 16:45:56 +0900

    commit

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-(function(){
+(async function(){
     console.log("hello treasure");
+    let localStream;
+    try{
+        localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        const videoElement = document.getElementById('my-video');
+        videoElement.srcObject = localStream;
+        videoElement.play();
+    } catch (error) {
+        alert(error)
+    }
+    //Peer作成
+    const peer = new Peer({
+        key: 'f43b2465-bc02-4d13-b7af-ed584aa2451e',
+        debug: 3
+    });
+    //PeerID取得
+    peer.on('open', () => {
+        document.getElementById('my-id').textContent = peer.id;
+    });
+
+    // 発信処理
+    document.getElementById('make-call').onclick = () => {
+        const theirID = document.getElementById('their-id').value;
+        const mediaConnection = peer.call(theirID, localStream);
+        setEventListener(mediaConnection);
+    };
+
+    // イベントリスナを設置する関数
+    const setEventListener = mediaConnection => {
+        mediaConnection.on('stream', stream => {
+            // video要素にカメラ映像をセットして再生
+            const videoElm = document.getElementById('their-video')
+            videoElm.srcObject = stream;
+            videoElm.play();
+        });
+    }
+
+    //着信処理
+    peer.on('call', mediaConnection => {
+        mediaConnection.answer(localStream);
+        setEventListener(mediaConnection);
+    });
+
 })();
