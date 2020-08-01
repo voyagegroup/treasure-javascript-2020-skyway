b91914affe5a107b15c66d5401d66cf017c3e9f9
t-hoso <taishihosokawa@yahoo.co.jp>
2020-08-01 00:47:00 -0700

    add call/recieve features

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 (async function(){
     console.log("hello treasure");
+    let localStream;
     try {
-        const localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
         const videoElement = document.getElementById('my-video');
         videoElement.srcObject = localStream;
         videoElement.play();
     } catch(error) {
         alert(error);
     }
+    const peer = new Peer({
+        key: '156b47be-f764-4fd5-b770-d2908290d788',
+        debug: 3
+      });
+
+    peer.on('open', () => {
+        document.getElementById('my-id').textContent = peer.id;
+    });
+
+    document.getElementById('make-call').onclick = () => {
+        const theirID = document.getElementById('their-id').value;
+        const mediaConnection = peer.call(theirID, localStream);
+        setEventListener(mediaConnection);
+      };
+      
+      // イベントリスナを設置する関数
+      const setEventListener = mediaConnection => {
+        mediaConnection.on('stream', stream => {
+          // video要素にカメラ映像をセットして再生
+          const videoElm = document.getElementById('their-video')
+          videoElm.srcObject = stream;
+          videoElm.play();
+        });
+      }
+
+    peer.on('call', mediaConnection => {
+        mediaConnection.answer(localStream);
+        setEventListener(mediaConnection);
+      });
 })();
