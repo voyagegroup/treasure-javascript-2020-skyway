8a82621072cfdb96283242b7d87aba6863b29b38
Daichi Kudo <sakanactor624@gmail.com>
2020-08-03 11:39:01 +0900

    :sparkles: mute audio and video

--- index.js
+++ index.js
@@ -9,21 +9,34 @@
   const disconnectTrigger = document.getElementById("js-disconnect-trigger");
   const permitTrigger = document.getElementById("js-permit-trigger");
   const rejectTrigger = document.getElementById("js-reject-trigger");
+  const cameraTrigger = document.getElementById("js-camera-trigger");
+  const cameraStatus = document.getElementById("js-camera-status");
+  const microphoneTrigger = document.getElementById("js-microphone-trigger");
+  const microphoneStatus = document.getElementById("js-microphone-status");
 
   // chat
   const localId = document.getElementById("js-local-id");
   const localText = document.getElementById("js-local-text");
   const closeTrigger = document.getElementById("js-close-trigger");
   const sendTrigger = document.getElementById("js-send-trigger");
-  const remoteId = document.getElementById("js-remote-id");
   const messages = document.getElementById("js-messages");
 
   // localStream ... ビデオ、音声データ
-  const localStream = await navigator.mediaDevices
+  let localStream = await navigator.mediaDevices
     .getUserMedia({ video: true, audio: true })
     .catch((err) => alert("mediaDevice.getUserMedia() error:", err));
   myVideo.srcObject = localStream;
   myVideo.play();
+
+  cameraTrigger.addEventListener("click", () => {
+    localStream.getVideoTracks()[0].enabled = !localStream.getVideoTracks()[0].enabled;
+    cameraStatus.textContent = localStream.getVideoTracks()[0].enabled ? "ON" : "OFF";
+  });
+
+  microphoneTrigger.addEventListener("click", () => {
+    localStream.getAudioTracks()[0].enabled = !localStream.getAudioTracks()[0].enabled;
+    microphoneStatus.textContent = localStream.getAudioTracks()[0].enabled ? "ON" : "OFF";
+  });
 
   const peer = new Peer({
     key: "1212399c-448f-4135-89d3-76deff99795a",
@@ -34,9 +47,15 @@
   peer.once("open", (id) => (localId.textContent = id));
 
   // 相手から接続要求がきた時
-  peer.on("call", (mediaConnection) => {
-    mediaConnection.answer(localStream);
-    setEventListener(mediaConnection);
+  peer.on("call", async (mediaConnection) => {
+    if (confirm("相手が見つかりました！\n接続しますか？")) {
+      // default: audio muted
+      localStream.getAudioTracks()[0].enabled = false;
+      mediaConnection.answer(localStream);
+      setEventListener(mediaConnection);
+    }
+    console.log(localStream.enabled);
+
     // permit
     // permitTrigger.onclick = () => {
     //   mediaConnection.answer(localStream);
@@ -120,7 +139,7 @@
     });
 
     // Register closing handler
-    closeTrigger.addEventListener("click", () => dataConnection.close(), {
+    disconnectTrigger.addEventListener("click", () => dataConnection.close(), {
       once: true,
     });
 
