3d0c0ea6ef6165151ca70488314f12ada8b7993a
Ogijun2018 <ogijun.mk@hotmail.co.jp>
2020-08-04 15:17:17 +0900

    チャット機能完成

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 (async function(){
-    console.log("hello treasure");
     let localStream;
     try{
         localStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
@@ -22,9 +21,45 @@
     });
 
     // 発信処理
-    document.getElementById('make-call').onclick = () => {
+    document.getElementById('js-connect-trigger').onclick = () => {
+        if (!peer.open) {
+            return;
+        }
+        
         const theirID = document.getElementById('their-id').value;
+        //ビデオ通話の開始
         const mediaConnection = peer.call(theirID, localStream);
+        //チャットの開始
+        const dataConnection = peer.connect(theirID);
+        dataConnection.once('open', async () => {
+            document.getElementById('js-messages').innerHTML += `=== DataConnection has been opened ===\n`;
+            document.getElementById('js-send-trigger').addEventListener('click', onClickSend);
+        });
+        
+        dataConnection.on('data', data => {
+            document.getElementById('js-messages').textContent += `Remote: ${data}\n`;
+        });
+    
+        dataConnection.once('close', () => {
+            document.getElementById('js-messages').textContent += `=== DataConnection has been closed ===\n`;
+            document.getElementById('js-send-trigger').removeEventListener('click', onClickSend);
+        });
+    
+        // Register closing handler
+        document.getElementById('js-close-trigger').addEventListener('click', () => {dataConnection.close(), {
+            once: true,
+        }, mediaConnection.close(true)});
+    
+        function onClickSend() {
+            const data = document.getElementById('js-local-text').value;
+            dataConnection.send(data);
+        
+            document.getElementById('js-messages').textContent += `You: ${data}\n`;
+            document.getElementById('js-messages').value = '';
+            console.log('clicked');
+        }
+
+
         setEventListener(mediaConnection);
     };
 
@@ -32,6 +67,36 @@
     peer.on('call', mediaConnection => {
         mediaConnection.answer(localStream);
         setEventListener(mediaConnection);
+    });
+
+    // Register connected peer handler
+    peer.on('connection', dataConnection => {
+        dataConnection.once('open', async () => {
+        document.getElementById('js-messages').innerHTML += `=== DataConnection has been opened ===\n`;
+        document.getElementById('js-send-trigger').addEventListener('click', onClickSend);
+        });
+
+        dataConnection.on('data', data => {
+        document.getElementById('js-messages').innerHTML += `Remote: ${data}\n`;
+        });
+
+        dataConnection.once('close', () => {
+        document.getElementById('js-messages').innerHTML += `=== DataConnection has been closed ===\n`;
+        document.getElementById('js-send-trigger').removeEventListener('click', onClickSend);
+        });
+
+        // Register closing handler
+        document.getElementById('js-close-trigger').addEventListener('click', () => {dataConnection.close(), {
+        once: true,
+        }, mediaConnection.close(true)});
+
+        function onClickSend() {
+        const data = document.getElementById('js-local-text').value;
+        dataConnection.send(data);
+
+        document.getElementById('js-messages').innerHTML += `You: ${data}\n`;
+        document.getElementById('js-messages').value = '';
+        }
     });
     
     // イベントリスナを設置する関数
