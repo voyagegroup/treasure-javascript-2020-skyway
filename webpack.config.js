c8753d43d498ec95eb19b60d2f6835c84e0984ab
Daichi Kudo <sakanactor624@gmail.com>
2020-08-05 17:34:43 +0900

    :wrench: update pkg, build to docs

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: "development",
   entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "bundle.js",
   },
   devServer: {
