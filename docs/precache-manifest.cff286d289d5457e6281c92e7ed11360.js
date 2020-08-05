e59f580d825f6b180ce78a54817e854f49294c0f
tokoroten-lab <tokoroten.lab@gmail.com>
2020-08-05 22:39:36 +0900

    Fix settings of hosting

---
+++ docs/precache-manifest.cff286d289d5457e6281c92e7ed11360.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e574e1324d125008838c30b66f4ff2b5",
+    "url": "/treasure-javascript-2020-skyway/index.html"
+  },
+  {
+    "revision": "2f0c357b4b207945288c",
+    "url": "/treasure-javascript-2020-skyway/static/css/main.49543553.chunk.css"
+  },
+  {
+    "revision": "acb09b89fe7698bee019",
+    "url": "/treasure-javascript-2020-skyway/static/js/2.0711ff93.chunk.js"
+  },
+  {
+    "revision": "07a501d9903b06542e25152959ed155c",
+    "url": "/treasure-javascript-2020-skyway/static/js/2.0711ff93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f0c357b4b207945288c",
+    "url": "/treasure-javascript-2020-skyway/static/js/main.b7a97fef.chunk.js"
+  },
+  {
+    "revision": "ba01988b9ba760bcf2ed",
+    "url": "/treasure-javascript-2020-skyway/static/js/runtime-main.52a76cba.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0880eade82d661628dd5936d56542a4b.js"
+  "/treasure-javascript-2020-skyway/precache-manifest.cff286d289d5457e6281c92e7ed11360.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/treasure-javascript-2020-skyway/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
