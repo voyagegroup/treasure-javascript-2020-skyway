cedd72c803344db444b9e3a28df9bd13f16e7d2c
Daichi Kudo <sakanactor624@gmail.com>
2020-08-05 22:23:26 +0900

    :wrench: move files and fix config

--- index.tsx
+++ index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./components/App";
+import { App } from "./src/App";
 
 // とりあえずskywayの読み込み
-import "./skyway";
+import "./src/skyway";
 
 const target = document.getElementById("app");
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.tsx",
+  entry: "./index.tsx",
   output: {
     path: path.resolve(__dirname, "docs"),
     filename: "bundle.js",
@@ -37,7 +37,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: "index.html",
     }),
   ],
   resolve: {
