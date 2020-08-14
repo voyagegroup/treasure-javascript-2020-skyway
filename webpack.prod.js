b74ad8da0797ef88463954a5a70c6d24efae742c
Daichi Kudo <sakanactor624@gmail.com>
2020-08-14 18:59:58 +0900

    :wrench: fix importing dotenv-webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = merge(common, {
   mode: "production",
